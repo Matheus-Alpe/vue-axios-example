0164d28dea3df89e8fe7021fadc19d93eb78fbed
Matheus Alves Pereira <matttalves@gmail.com>
2021-08-21 19:20:47 -0300

    feat: base components

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
-    <div id="app">
+  <div id="app">
+    
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Requisições HTTP no Vue</h1>
+        <p class="lead">Usando a biblioteca Axios para fazer chamadas Ajax à uma API REST.</p>
+      </div>
+    </div>
 
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Requisições HTTP no Vue</h1>
-                <p class="lead">Usando a biblioteca Axios para fazer chamadas Ajax à uma API REST.</p>
-            </div>
-        </div>
+    <div class="container">
 
-        <div class="container">
-            
-        </div>
+      <TarefasLista />
 
     </div>
+
+  </div>
 </template>
 
 <script>
-export default {
 
+import TarefasLista from './components/TarefasLista.vue'
+
+export default {
+    components: {
+        TarefasLista
+    }
 }
 </script>
-
-<style>
-</style>
