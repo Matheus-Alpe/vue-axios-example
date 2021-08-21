56f7c94bcd4339c3c41517152535808629511a6c
Matheus Alves Pereira <matttalves@gmail.com>
2021-08-21 20:07:34 -0300

    feat: axios post

--- src/components/TarefaSalvar.vue
+++ src/components/TarefaSalvar.vue
@@ -2,7 +2,7 @@
     <div class="mt-4">
         <hr>
         <h2 class="font-weight-light">Salvar Tarefa</h2>
-        <form>
+        <form @submit.prevent="salvar">
             <div class="row">
                 <div :class="classeColuna">
                     <div class="form-group">
@@ -10,7 +10,8 @@
                         <input 
                             type="text"
                             class="form-control" 
-                            placeholder="Título da tarefa">
+                            placeholder="Título da tarefa"
+                            v-model="tarefaLocal.titulo">
                     </div>
                 </div>
                 <div class="col-sm-2" v-if="tarefa">
@@ -29,6 +30,8 @@
 </template>
 
 <script>
+const TAREFA_PADRAO = { titulo: '', concluido: false }
+
 export default {
     props: {
         tarefa: {
@@ -38,12 +41,18 @@ export default {
     },
     data() {
         return {
-            tarefaLocal: Object.assign({}, { titulo: '', concluido: false }, this.tarefa)
+            tarefaLocal: Object.assign({}, TAREFA_PADRAO, this.tarefa)
         }
     },
     computed: {
         classeColuna() {
             return this.tarefa ? 'col-sm-10' : 'col-sm-12'
+        }
+    },
+    methods: {
+        salvar() {
+            this.$emit('criar', this.tarefaLocal)
+            this.tarefaLocal = Object.assign({}, TAREFA_PADRAO)
         }
     }
 }
--- src/components/TarefasLista.vue
+++ src/components/TarefasLista.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
 
-        <h1 class="font-weight-light">Lista de Tarefas</h1>
+        <div class="row">
+            <div class="col-sm-10">
+                <h1 class="font-weight-light">Lista de Tarefas</h1>
+            </div>
+            <div class="col-sm-2">
+                <button class="btn btn-primary float-right" @click="exibirFormulario = !exibirFormulario">
+                    <i class="fa fa-plus mr-2"></i>
+                    <span>Criar</span>
+                </button>
+            </div>
+        </div>
 
         <ul class="list-group" v-if="tarefas.length">
             <TarefasListaIten
@@ -12,7 +22,9 @@
 
         <p v-else>Nenhuma tarefa criada.</p>
 
-        <TarefaSalvar />
+        <TarefaSalvar 
+            v-if="exibirFormulario"
+            @criar="criarTarefa"/>
 
     </div>
 </template>
@@ -32,13 +44,26 @@ export default {
     },
     data() {
         return {
-            tarefas: []
+            tarefas: [],
+            exibirFormulario: false
+        }
+    },
+
+    methods: {
+        criarTarefa(tarefa) {
+            axios.post(`${config.apiURL}/tarefas`, tarefa)
+                .then((response) => {
+                    console.log('POST /tarefas', response)
+                    this.tarefas.push(response.data)
+                    this.exibirFormulario = false
+                })
         }
     },
 
     created() {
         axios.get(`${config.apiURL}/tarefas`)
             .then((response) => {
+                console.log('GET /tarefas', response)
                 this.tarefas = response.data
             })
     }
