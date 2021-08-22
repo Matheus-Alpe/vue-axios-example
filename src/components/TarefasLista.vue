db7083beab058b0e17db795ec1000488420af2ed
Matheus Alves Pereira <matttalves@gmail.com>
2021-08-22 13:20:49 -0300

    fix: task order

--- src/components/TarefasLista.vue
+++ src/components/TarefasLista.vue
@@ -15,7 +15,7 @@
 
         <ul class="list-group" v-if="tarefas.length">
             <TarefasListaIten
-                v-for="tarefa in tarefas"
+                v-for="tarefa in tarefasOrdenadas"
                 :key="tarefa.id"
                 :tarefa="tarefa"
                 @editar="selecionarTarefaParaEdicao"
@@ -52,6 +52,21 @@ export default {
             tarefas: [],
             exibirFormulario: false,
             tarefaSelecionada: undefined
+        }
+    },
+
+    computed: {
+        tarefasOrdenadas() {
+            return this.tarefas.slice().sort((t1, t2) => {
+                if (t1.concluido === t2.concluido) {
+                    return t1.titulo < t2.titulo
+                        ? -1
+                        : t1.titulo > t2.titulo 
+                            ? 1
+                            : 0
+                }
+                return t1.concluido - t2.concluido
+            })
         }
     },
 
